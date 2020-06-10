b7de0bcc47a0ab1f978d2336ebd67e8ba9532538
liangliang <1035847702@qq.com>
2020-06-10 19:20:58 +0800

    init_cmd_qrcode

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import sys
 
 
 if __name__ == "__main__":
-    epidemic_prevention = EpidemicPrevention(bool(sys.argv[1]))
+    epidemic_prevention = EpidemicPrevention(bool(sys.argv[1] if len(sys.argv) > 1 else ""))
